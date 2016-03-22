28153df1bb1836a651908fa5643bdc77fba19fcd
billythekid <billy@the-kid.org>
2016-03-22 14:49:51 +0000

    changed to v-on from @ because xml ;o/

--- resources/views/abzphp.blade.php
+++ resources/views/abzphp.blade.php
@@ -14,7 +14,7 @@
                     <div class="form-group">
                         <label for="newContestant">Name:</label>
                         <input class="form-control" id="newContestant" v-model="newContestant"
-                               @keyup.enter="addContestant">
+                               v-on:keyup.enter="addContestant">
                     </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@
                 <div class="col-md-9">
                     <div class="form-group">
                         <label for="newPrize">Name:</label>
-                        <input class="form-control" id="newPrize" v-model="newPrize" @keyup.enter="addPrize">
+                        <input class="form-control" id="newPrize" v-model="newPrize" v-on:keyup.enter="addPrize">
                     </div>
                 </div>
 
@@ -39,44 +39,46 @@
 
             <div class="col-md-2">
                 <div class="form-group">
-                    <div class="prizeButton img-rounded btn-success text-center" @click="pickWinners">
-                    <i class="fa fa-5x fa-random"></i>
+                    <div class="prizeButton img-rounded btn-success text-center" v-on:click="pickWinners">
+                        <i class="fa fa-5x fa-random"></i>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-5">
-            <p v-for="contestant in contestants">
-                <button class="btn btn-danger" @click="removeContestant($index)"><i class="fa fa-2x fa-trash"></i>
-                </button>
-                @{{ contestant }}
-            </p>
-        </div>
-        <div class="col-md-5">
-            <p v-for="prize in prizes">
-                <button class="btn btn-danger" @click="removePrize($index)"><i class="fa fa-2x fa-trash"></i>
-                </button>
-                @{{ prize.name }}
-            </p>
-        </div>
-
-        <div class="results panel panel-success" v-if="winners.length">
-            <div class="panel-heading">
-                <h1>TONIGHT'S WINNERS!
-                    <button class="close" @click='clearWinners'>&times;</button></h1>
+            <div class="col-md-5">
+                <p v-for="contestant in contestants">
+                    <button class="btn btn-danger" v-on:click="removeContestant($index)"><i
+                                class="fa fa-2x fa-trash"></i>
+                    </button>
+                    @{{ contestant }}
+                </p>
             </div>
-            <table class="table table-hover">
-                <tr v-for="winner in winners">
-                    <td>
-                        <i class="fa fa-2x fa-gift"></i> <span class="lead">@{{ winner.prize.name }}</span>
-                    </td>
-                    <td>
-                        <i class="fa fa-2x fa-user"></i> <span class="lead">@{{ winner.name }}</span>
-                    </td>
-                </tr>
-            </table>
+            <div class="col-md-5">
+                <p v-for="prize in prizes">
+                    <button class="btn btn-danger" v-on:click="removePrize($index)"><i class="fa fa-2x fa-trash"></i>
+                    </button>
+                    @{{ prize.name }}
+                </p>
+            </div>
+
+            <div class="results panel panel-success" v-if="winners.length">
+                <div class="panel-heading">
+                    <h1>TONIGHT'S WINNERS!
+                        <button class="close" v-on:click='clearWinners'>&times;</button>
+                    </h1>
+                </div>
+                <table class="table table-hover">
+                    <tr v-for="winner in winners">
+                        <td>
+                            <i class="fa fa-2x fa-gift"></i> <span class="lead">@{{ winner.prize.name }}</span>
+                        </td>
+                        <td>
+                            <i class="fa fa-2x fa-user"></i> <span class="lead">@{{ winner.name }}</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
-    </div>
     </div>
 @endsection
 
