a711ca1c7d60dc37efe9f9737ad9df3b748cad73
billythekid <billy@the-kid.org>
2016-07-07 23:32:54 +0100

    fixed welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,30 @@
 <html>
 <head>
     <title>the-kid.org</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <meta name=viewport content="width=device-width, initial-scale=1">
     <style>
         html, body {
             height: 100%;
         }
 
         body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
+            margin:      0;
+            padding:     0;
+            width:       100%;
+            display:     table;
             font-weight: 100;
-            font-family: 'Lato', sans-serif;
+            font-family: sans-serif;
         }
 
         .container {
-            text-align: center;
-            display: table-cell;
+            text-align:     center;
+            display:        table-cell;
             vertical-align: middle;
         }
 
         .content {
             text-align: center;
-            display: inline-block;
+            display:    inline-block;
         }
 
         .title {
